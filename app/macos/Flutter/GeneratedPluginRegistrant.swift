b6e21b08dbfc3c84846ace6ed70dbced89dda532
amine2077 <missaouimohammed64@gmail.com>
2026-06-20 22:04:21 +0100

    Update generated plugin registrants for new dependencies
    Auto-generated by flutter pub get after adding shared_preferences,
    launch_at_startup, tray_manager, and local_notifier.

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import local_notifier
 import screen_retriever_macos
+import shared_preferences_foundation
+import tray_manager
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  LocalNotifierPlugin.register(with: registry.registrar(forPlugin: "LocalNotifierPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
